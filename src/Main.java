411e7ae6b5049454c2664eeb2f05b92a81a5a225
680615527MinSwanPyei <minswanpyae10@gmail.com>
2026-07-06 00:57:11 +0700

    Add umbrella drawing code

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        Turtle myUmbrella = new Turtle();
+        myUmbrella.speed(5);
+
+        myUmbrella.penColor("yellow");
+        myUmbrella.width(3);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(-120, 100);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(-80, 130);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(-40, 145);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(0, 150);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(40, 145);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(80, 130);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(120, 100);
+        myUmbrella.down();
+        myUmbrella.dot("yellow", 110);
+
+        myUmbrella.penColor("yellow");
+        myUmbrella.width(90);
+        myUmbrella.up();
+        myUmbrella.setPosition(-120, 90);
+        myUmbrella.down();
+        myUmbrella.forward(240);
+
+        myUmbrella.penColor("black");
+        myUmbrella.width(4);
+        myUmbrella.up();
+        myUmbrella.setPosition(-130, 45);
+        myUmbrella.down();
+        myUmbrella.forward(260);
+
+        myUmbrella.up();
+        myUmbrella.setPosition(0, 45);
+        myUmbrella.right(90);
+        myUmbrella.down();
+        myUmbrella.forward(150);
+
+        for (int i = 0; i < 18; i++) {
+            myUmbrella.forward(5);
+            myUmbrella.left(10);
+        }
+    }
 }
